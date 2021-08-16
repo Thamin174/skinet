dfab30b0b839fcd0df76148e92ff4427902a220d
THAMINNWIN10\Thamin Naik <thaminn@mindfiresolutions.com>
2021-08-17 01:12:11 +0530

    Pagination,Filter,Sorting and Cors policy implimented

--- API/Startup.cs
+++ API/Startup.cs
@@ -44,6 +44,14 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+               services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +72,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
