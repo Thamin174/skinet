2d71e3517420c577a1077235497389b01234c485
THAMINNWIN10\Thamin Naik <thaminn@mindfiresolutions.com>
2021-08-26 08:14:23 +0530

    Order Summary Implemented

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -21,3 +21,9 @@ export class Basket implements IBasket {
     items: IBasketItem[] = [];
 
 }
+
+export interface IBasketTotal {
+    shipping: number;
+    subtotal: number;
+    total: number;
+}
