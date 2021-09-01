74150c34ad8049314fcc32ac150a658287efd97a
THAMINNWIN10\Thamin Naik <thaminn@mindfiresolutions.com>
2021-09-01 22:58:40 +0530

    Validation server side implimented

---
+++ API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Core.Entities;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+        public string Id { get; set; }
+        
+        public List<BasketItem> Items {get; set;}
+        
+    }
+}
